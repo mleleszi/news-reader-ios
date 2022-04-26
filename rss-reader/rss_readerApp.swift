f24aca046a1bdb5c9a56c7ff7e625e94e3ecb714
mleleszi <mleleszi@icloud.com>
2022-04-26 09:07:32 +0200

    initial commit

---
+++ rss-reader/rss_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  rss_readerApp.swift
+//  rss-reader
+//
+//  Created by Marcell Leleszi on 2022. 04. 26..
+//
+
+import SwiftUI
+
+@main
+struct rss_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
