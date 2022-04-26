5489f846a0f00a9c1ffae93aeca7ee7d7b285917
mleleszi <mleleszi@icloud.com>
2022-04-26 12:59:31 +0200

    article source custom initializers, example func

--- rss-reader/model/Article.swift
+++ rss-reader/model/Article.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Article: Decodable, Identifiable {
-    let id = UUID()
+    let id: UUID
     let source: Source
     let author: String?
     let title: String?
@@ -17,4 +17,46 @@ struct Article: Decodable, Identifiable {
     let urlToImage: String?
     let publishedAt: String?
     let content: String?
+    
+    enum CodingKeys: CodingKey {
+        case source, author, title, description, url, urlToImage, publishedAt, content
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = UUID()
+        source = try values.decode(Source.self, forKey: .source)
+        author = try values.decode(String.self, forKey: .author)
+        title = try values.decode(String.self, forKey: .title)
+        description = try values.decode(String.self, forKey: .description)
+        url = try values.decode(String.self, forKey: .url)
+        urlToImage = try values.decode(String.self, forKey: .urlToImage)
+        publishedAt = try values.decode(String.self, forKey: .publishedAt)
+        content = try values.decode(String.self, forKey: .content)
+    }
+    
+    init(source: Source, author: String, title: String?, description: String?, url: String?, urlToImage: String?, publishedAt: String?, content: String?) {
+        id = UUID()
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+    }
+    
+    static func returnExampleArticle() -> Article {
+        return Article(source: Source(id: nil, name: "The Guardian"),
+                       author: "Tumaini Carayol",
+                       title: "Emma Raducanu searching for fourth coach after split with Torben Beltz",
+                       description: "<ul><li>Beltz’s experience had limited effect in five-month stay</li><li>Recent weeks had seen positive uplift for British player</li></ul>Emma Raducanu has split with her coach, Torben Beltz, after working together for only five months. Citing the need to be…",
+                       url: "https://amp.theguardian.com/sport/2022/apr/26/emma-raducanu-searching-for-fourth-coach-after-split-with-torben-beltz-tennis",
+                       urlToImage: "https://i.guim.co.uk/img/media/10e972f75e0cc0d30bcf191626544df826c6cfef/0_26_4700_2820/master/4700.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=2882432f878fa8df919a45dd31fbf775",
+                       publishedAt: "2022-04-26T07:43:36Z",
+                       content: "Emma Raducanu has split with her coach, Torben Beltz, after working together for only five months. Citing the need to begin a new training model, Raducanu will once again be searching for a new coach… [+3231 chars]")
+    }
+
 }
--- rss-reader/model/Source.swift
+++ rss-reader/model/Source.swift
@@ -10,4 +10,19 @@ import Foundation
 struct Source: Decodable {
     let id: String?
     let name: String
+    
+    enum CodingKeys: CodingKey {
+        case id, name
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        name = try values.decode(String.self, forKey: .name)
+    }
+    
+    init(id: String?, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
